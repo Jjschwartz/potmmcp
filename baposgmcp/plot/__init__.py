9c05975e8547814b50a9811778ca06c7e3aa2c37
Jonathon <Jonathon.schwartz12@gmail.com>
2022-10-16 08:21:01 -0400

    fixed plot utils import

--- baposgmcp/plot/__init__.py
+++ baposgmcp/plot/__init__.py
@@ -6,12 +6,13 @@ from baposgmcp.plot.expected import get_uniform_expected_df
 from baposgmcp.plot.expected import plot_expected_values_by_num_sims
 from baposgmcp.plot.heatmap import annotate_heatmap
 from baposgmcp.plot.heatmap import heatmap
-from baposgmcp.plot.heatmap import plot_pairwise_heatmap
 from baposgmcp.plot.pairwise import get_pairwise_values
 from baposgmcp.plot.pairwise import plot_pairwise_comparison
+from baposgmcp.plot.heatmap import plot_pairwise_heatmap
 from baposgmcp.plot.pairwise import plot_pairwise_population_comparison
+from baposgmcp.plot.utils import add_df_coplayer_policy_id
+from baposgmcp.plot.utils import add_df_multiple_coplayer_policy_id
 from baposgmcp.plot.utils import filter_by
 from baposgmcp.plot.utils import filter_exps_by
-from baposgmcp.plot.utils import add_df_coplayer_policy_id
 from baposgmcp.plot.utils import import_results
 from baposgmcp.plot.utils import plot_environment
